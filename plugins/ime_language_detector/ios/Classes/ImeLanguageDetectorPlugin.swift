142be6067d43d5e7f58ba34543131286bcfbe5d6
cvbmgh <cvbmgh@126.com>
2025-10-13 17:34:20 +0800

    commit

--- plugins/ime_language_detector/ios/Classes/ImeLanguageDetectorPlugin.swift
+++ plugins/ime_language_detector/ios/Classes/ImeLanguageDetectorPlugin.swift
@@ -12,8 +12,37 @@ public class ImeLanguageDetectorPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+    case "getImeLanguages":
+      let languages = getImeLanguage()
+      result([languages])
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+
+  private func getImeLanguage() -> String {
+    // 创建一个临时输入框
+    let textField = UITextField()
+    UIApplication.shared.windows.first?.addSubview(textField)
+    textField.becomeFirstResponder()
+
+    // 等待系统分配 inputMode（必须在主线程）
+    let lang = textField.textInputMode?.primaryLanguage ?? ""
+
+    // 清理
+    textField.resignFirstResponder()
+    textField.removeFromSuperview()
+
+    return lang
+  }
+
+  // 获取软键盘语言列表的第一个语言
+//   private func getImeLanguage() -> String {
+//       if let inputMode = UITextInputMode.activeInputModes.first,
+//          let lang = inputMode.primaryLanguage {
+//           return lang
+//       }
+//       return ""
+//   }
+
 }
